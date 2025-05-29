c4534737d847fbaa4bfd092534b907039ae0c947
Sharier Khan <mrkhansharier@gmail.com>
2025-05-29 13:11:36 -0230

    Add new field hiveKey in the databse for new entry

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -6,20 +6,20 @@ part 'expense.g.dart'; // Generated file
 class Expense {
   @HiveField(0)
   final String description;
-
   @HiveField(1)
   final double amount;
-
   @HiveField(2)
   final DateTime date;
-
   @HiveField(3)
   final String category;
+  @HiveField(4)
+  final int? hiveKey; // Add this nullable field
 
   Expense({
     required this.description,
     required this.amount,
     required this.date,
     required this.category,
+    this.hiveKey, // Will be null for new expenses
   });
 }
